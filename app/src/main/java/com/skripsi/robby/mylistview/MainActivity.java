d0218680f2471b5541c5c3475131bbe778333842
robbydhr <robby.danu@gmail.com>
2019-11-29 09:05:30 +0700

    Initial commit

---
+++ app/src/main/java/com/skripsi/robby/mylistview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.skripsi.robby.mylistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara","Moh Yamin","Patimura","R A Kartini","Sukarno"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.lv_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
+        listView.setAdapter(adapter);
+    }
+}
